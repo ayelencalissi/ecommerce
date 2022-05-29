665e6388159cddcebd0f6873bf142ff373eef771
ayelencalissi <ayelencalissi@gmail.com>
2022-05-29 02:14:20 -0300

    resolviendo bug al leer property undefined

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,21 +1,32 @@
+import { useEffect, useState } from "react"
 import { Container, Row, Col, Card } from "react-bootstrap"
 const ItemDetail = (props) => {
 
-    const {title, description, price, pictureUrl} = props.product
+   //const {title, description, price, pictureUrl} = props?.product
+    const [product, setProduct] = useState({
+        title: '',
+        description: '',
+        price: 0,
+        pictureUrl: ''
+    })
+
+    useEffect(() =>{
+        setProduct(props.product)
+    },[props.product])
 
     return (
         <Container>
             <Row>
                 <Col md={{ span: 4, offset: 4 }}>
                     <Card style={{ width: '100%' }}>
-                    <Card.Img variant="top" src={pictureUrl} />
+                    <Card.Img variant="top" src={product?.pictureUrl} />
                         <Card.Body>
-                            <Card.Title>{title}</Card.Title>
+                            <Card.Title>{product?.title}</Card.Title>
                             <Card.Text>
-                                {description}
+                                {product?.description}
                             </Card.Text>
                         </Card.Body>
-                        <Card.Footer className="text-muted">${price}</Card.Footer>
+                        <Card.Footer className="text-muted">${product?.price}</Card.Footer>
                     </Card>
                 </Col>
             </Row>
